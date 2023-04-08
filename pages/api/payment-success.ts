f6516484b4890bfa96e342c84cfa0d116129dfa4
Tanmay Gupta <tg1482@stern.nyu.edu>
2023-04-08 16:27:41 -0700

    redundant

--- pages/api/payment-success.ts
+++ pages/api/payment-success.ts
@@ -15,7 +15,6 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
             // Declare variables
             let plan,price,queriesAllowed,queriesMade;
 
-            // if planId is 1, then plan = Free, price = 0, queriesAllowed = 10
             // if planId is 2, then plan = Premium, price = 5, queriesAllowed = 50
 
             plan = 'Premium';
@@ -28,11 +27,6 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
                 'INSERT INTO SubscriptionFact (createdAt, updatedAt, planId, plan, userId, active, price, queriesAllowed, queriesMade) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
                 [new Date(),new Date(),planId,plan,userId,1,price,queriesAllowed,queriesMade]
             );
-            // const subscriptionId = (result as any).insertId;
-
-            // return res.status(201).json({ subscriptionId });
-
-            // Sign the user in (you will need to implement this)
 
             // Redirect the user to the home page
             res.writeHead(302,{ Location: '/' });
